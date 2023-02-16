4e7689251723c69152633f2e2f504c016c14ffc7
Andrew <andrew0189@yandex.ru>
2023-02-16 13:46:05 +0500

    commit first

---
+++ GenPass/Models/Passwrd.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenPass.Models
+{
+    internal class Passwrd
+    {
+        public int LenPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
---
+++ GenPass/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GenPass.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenPass.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок
+        /// <summary> Заголовок приложения </summary>
+        private string _Title = "GenPass";
+
+        /// <summary> Заголовок приложения </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+        private string _Logotip = "/Views/Windows/logopass.png";
+
+        public string Logotip
+        {
+            get => _Logotip;
+            set => Set(ref _Logotip, value);
+        }
+
+
+    }
+}
